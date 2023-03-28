1f6e1ef6482fc267d3ce4765829d3ff4519c5a6f
kapypara <ahmed96s@hotmail.com>
2023-03-28 21:30:07 +0300

    [Model/Item]: Refined class structure and init methods.

--- Refur/Model/Item.swift
+++ Refur/Model/Item.swift
@@ -4,49 +4,103 @@ enum ItemCondition {
 }
 
 enum ItemCategory {
-    case clothing, books, other
+    case Clothing, Book, Other
 }
 
-enum ItemType {
-    case tShirt, Pants, shoes, hats
-    
-    case paperback, hardcover
-    
-    case other, anitque
-}
-
-enum itemSize {
-    case medium, small, large, XL
-}
 
 class Item {
 
     var name: String
     var description: String
     
-    var condition: ItemCondition
     var price: Double
+    var condition: ItemCondition
     
-    var category: ItemCategory
-    var type: ItemType
-    var size: itemSize
+    let category: ItemCategory
+
+    // the type and size will be set depending on the category
+    let type: Any? = nil
+    let size: Any? = nil
     
-    init(name: String, description: String,
+    // MARK: Clothing init
+    static func Clothing(name: String, description: String,
+            condition: ItemCondition, price: Double,
+            type: ClothingType, size: ClothingSize) -> Item {
+
+        return Item(name: name, description: description,
+                condition: condition, price: price,
+                category: ItemCategory.Clothing, type: type, size: size)
+    }
+
+    // MARK: Shoe init
+    static func Shoes(name: String, description: String,
+            condition: ItemCondition, price: Double,
+            ShoeSize: Double) -> Item {
+
+        return Item(name: name, description: description,
+                condition: condition, price: price,
+                category: ItemCategory.Clothing, type: ClothingType.Shoes, size: ShoeSize)
+    }
+
+
+    // MARK: Book init
+    static func Book(name: String, description: String,
+            condition: ItemCondition, price: Double,
+            type: BookType) -> Item {
+
+        return Item(name: name, description: description,
+                condition: condition, price: price,
+                category: ItemCategory.Book, type: type, size: nil)
+    }
+
+
+    // MARK: Other init
+    static func Other(name: String, description: String,
+            condition: ItemCondition, price: Double,
+            type: OtherTYpe) -> Item {
+
+        return Item(name: name, description: description,
+                condition: condition, price: price,
+                category: ItemCategory.Other, type: type, size: nil)
+    }
+
+
+
+    private init(name: String, description: String,
          condition: ItemCondition, price: Double,
-         category: ItemCategory, type: ItemType, size:itemSize) {
+         category: Any, type: Any, size: Any) {
         
         self.name = name
         self.description = description
         
-        self.condition = condition
         self.price = price
+        self.condition = condition
         
         self.category = category
+
         self.type = type
         self.size = size
-        
     }
 }
 
-//let myItem = item()
+
+// MARK: category specific enum
+
+enum ClothingType {
+    case TShirt, Pants, Shoes, Hat
+}
+
+enum BookType {
+    case Paperback, Hardcover
+}
+    
+enum OtherTYpe {
+    case Other, Anitque
+}
+
+enum ClothingSize {
+    case medium, small, large
+
+    case XXS, XS, XL, XXL
+}
 
