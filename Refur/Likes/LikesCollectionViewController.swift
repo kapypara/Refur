0a36f1a730e1dbde05f91fff8bbc121b0ce9f27d
KapyPara <ahmed96s@hotmail.com>
2023-04-10 02:59:23 +0300

    [Likes]: the view now uses the user Liked Posts

--- Refur/Likes/LikesCollectionViewController.swift
+++ Refur/Likes/LikesCollectionViewController.swift
@@ -9,57 +9,101 @@ import UIKit
 
 private let reuseIdentifier = "Cell"
 
+private var loadedPost = false
+
 class LikesCollectionViewController: UICollectionViewController {
-
-    var arrayLikes: [Post] = []
-   
-    let profile = Profile(name: "big man", handle: "BigStuff", picture: "bday.png")
-
-     
-     override func viewDidLoad() {
-         super.viewDidLoad()
-         collectionView.delegate = self
-         collectionView.dataSource = self
-         
-         Database.Users.observeUser(user: "Gigi") { userProfile in
-             guard let userProfile = userProfile else { return }
-             
-             for post in userProfile.posts {
-                 Database.Posts.observePost(post: post) { post in
-                     if let post = post {
-                         self.arrayLikes.append(post)
-                         self.collectionView.reloadData()
-                     }
-                 }
-             }
-         }
-     }
-
-
-
-     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-         // #warning Incomplete implementation, return the number of items
-         return arrayLikes.count
-     }
-
-     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    
+    var postArray: [Post] = []
+    var profileDict: [String: Profile] = [:]
+    
+    var selectedPostIndex: Int = 0
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if User.isLoggedOut {
+            unwindIfNotLoggedIn(segueIdentifier: "toHome")
+        } else {
+            populatePostArray()
+        }
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+    }
+    
+    func populatePostArray() {
+        
+        guard !loadedPost else { return }
+        loadedPost = true
+        
+        Database.Users.observeUser(user: User.uid!) { userProfile in
+            guard let userProfile = userProfile else { return }
+            
+            self.postArray = []
+            
+            for post in userProfile.posts {
+                Database.Posts.observePost(post: post) { post in
+                    if let post = post {
+                        
+                        // at this step we have a newe post to display
+                        self.postArray.append(post)
+                        self.collectionView.reloadData()
+                        
+                        
+                        // if we find a new user that is not in the dict we added to it
+                        guard self.profileDict[post.userUuid] == nil else { return }
+                        Database.Users.observeUser(user: post.userUuid) { loadedProfile in
+                            if let profile = loadedProfile {
+                                
+                                self.profileDict[post.userUuid] = profile
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    // MARK: CollectionView Functions
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return postArray.count
+    }
+    
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        selectedPostIndex = indexPath.row
+        performSegue(withIdentifier: "showDetails", sender: self)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "LikesCell", for: indexPath) as! CategoriesCollectionViewCell
-     
-         // Configure the cell
-         let likes = arrayLikes[indexPath.row]
-         
-         cell.setupCall(post: likes)
-         return cell
-     }
-
+        
+        // Configure the cell
+        let likes = postArray[indexPath.row]
+        
+        cell.setupCall(post: likes)
+        return cell
+    }
+    
+    
+    // MARK: Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showDetails" {
             guard let viewController = segue.destination as? ItemDetailsViewController else { return }
             
-            viewController.userProfile = profile
-            viewController.userPost = arrayLikes[0]
+            let post = postArray[selectedPostIndex]
+            
+            viewController.userPost = post
+            viewController.userProfile = profileDict[post.userUuid]
         }
     }
-
-
+    
+    
 }
